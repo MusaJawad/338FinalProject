342bfc77ed5a5606f54eef618e942761382475b4
MusaJawad <musa.jawad@ucalgary.ca>
2023-04-06 00:28:11 -0600

    Committed Dnode

--- main/java/mylib/datastructures/nodes/Dnode.java
+++ main/java/mylib/datastructures/nodes/Dnode.java
@@ -1,5 +1,37 @@
 package main.java.mylib.datastructures.nodes;
 
-public class Dnode {
-    
+public class Dnode<T> {
+    private T data;
+    private Dnode<T> next;
+    private Dnode<T> prev;
+
+    public Dnode(T data) {
+        this.data = data;
+        this.next = null;
+        this.prev = null;
+    }
+
+    public T getData() {
+        return this.data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public Dnode<T> getNext() {
+        return this.next;
+    }
+
+    public void setNext(Dnode<T> next) {
+        this.next = next;
+    }
+
+    public Dnode<T> getPrev() {
+        return this.prev;
+    }
+
+    public void setPrev(Dnode<T> prev) {
+        this.prev = prev;
+    }
 }
