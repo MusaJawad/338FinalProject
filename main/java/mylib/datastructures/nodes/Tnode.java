f1eabeb16b592491115a44b722521e19c62db45c
ahmedi64 <ahmed.iqbal1@ucalgary.ca>
2023-04-06 00:57:49 -0600

    Tnode

--- main/java/mylib/datastructures/nodes/Tnode.java
+++ main/java/mylib/datastructures/nodes/Tnode.java
@@ -1,5 +1,40 @@
 package main.java.mylib.datastructures.nodes;
 
-public class Tnode {
-    
+public class Tnode<T> {
+    private T data;
+    private Tnode<T> left;
+    private Tnode<T> right;
+
+    // constructor
+    public Tnode(T data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+
+    // getters and setters
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public Tnode<T> getLeft() {
+        return left;
+    }
+
+    public void setLeft(Tnode<T> left) {
+        this.left = left;
+    }
+
+    public Tnode<T> getRight() {
+        return right;
+    }
+
+    public void setRight(Tnode<T> right) {
+        this.right = right;
+    }
 }
+
