0ef815d71d41ea76e76890d54a0fce8abc64eb20
MusaJawad <musa.jawad@ucalgary.ca>
2023-04-07 02:36:51 -0600

    Deleted Snode

--- main/java/mylib/datastructures/Linear/SLL.java
+++ main/java/mylib/datastructures/Linear/SLL.java
@@ -1,10 +1,10 @@
 package main.java.mylib.datastructures.Linear;
 
-import main.java.mylib.datastructures.nodes.SNode;
+import main.java.mylib.datastructures.nodes.DNode;
 
 public class SLL {
-    private SNode head;
-    private SNode tail;
+    private DNode head;
+    private DNode tail;
     private int size;
     private boolean sorted;
 
@@ -16,14 +16,14 @@ public class SLL {
         sorted = true;
     }
 
-    public SLL(SNode head) {
+    public SLL(DNode head) {
         this.head = head;
         this.tail = head;
         size = 1;
         sorted = true;
     }
 
-    public void insertHead(SNode node) {
+    public void insertHead(DNode node) {
         if (head == null) {
             head = node;
             tail = node;
@@ -35,7 +35,7 @@ public class SLL {
         sorted = false;
     }
 
-    public void insertTail(SNode node) {
+    public void insertTail(DNode node) {
         if (head == null) {
             head = node;
             tail = node;
@@ -47,7 +47,7 @@ public class SLL {
         sorted = false;
     }
 
-    public void insert(SNode node, int position) {
+    public void insert(DNode node, int position) {
         if (position < 1 || position > size + 1) {
             System.out.println("Invalid position");
             return;
@@ -60,7 +60,7 @@ public class SLL {
             insertTail(node);
             return;
         }
-        SNode current = head;
+        DNode current = head;
         for (int i = 1; i < position - 1; i++) {
             current = current.next;
         }
@@ -70,7 +70,7 @@ public class SLL {
         sorted = false;
     }
 
-    public void sortedInsert(SNode node) {
+    public void sortedInsert(DNode node) {
         if (!sorted) {
             sort();
         }
@@ -82,7 +82,7 @@ public class SLL {
             insertTail(node);
             return;
         }
-        SNode current = head;
+        DNode current = head;
         while (current.next != null && current.next.data < node.data) {
             current = current.next;
         }
@@ -91,8 +91,8 @@ public class SLL {
         size++;
     }
 
-    public SNode search(int data) {
-        SNode current = head;
+    public DNode search(int data) {
+        DNode current = head;
         while (current != null) {
             if (current.data == data) {
                 return current;
@@ -118,7 +118,7 @@ public class SLL {
             head = null;
             tail = null;
         } else {
-            SNode current = head;
+            DNode current = head;
             while (current.next != tail) {
                 current = current.next;
             }
@@ -128,7 +128,7 @@ public class SLL {
         size--;
     }
 
-    public void delete(SNode node) {
+    public void delete(DNode node) {
         if (head == null) {
             return;
         }
@@ -140,7 +140,7 @@ public class SLL {
             deleteTail();
             return;
         }
-        SNode current = head;
+        DNode current = head;
         while (current.next != node) {
             current = current.next;
         }
@@ -151,15 +151,15 @@ public class SLL {
         if (size <= 1) {
             return;
         }
-        SNode sortedHead = null;
-        SNode current = head;
+        DNode sortedHead = null;
+        DNode current = head;
         while (current != null) {
-            SNode next = current.next;
+            DNode next = current.next;
             if (sortedHead == null || current.data < sortedHead.data) {
                 current.next = sortedHead;
                 sortedHead = current;
             } else {
-                SNode search = sortedHead;
+                DNode search = sortedHead;
                 while (search.next != null && current.data > search.next.data) {
                     search = search.next;
                 }
@@ -182,7 +182,7 @@ public class SLL {
         System.out.println("List length: " + size);
         System.out.println("Sorted status: " + isSorted());
         System.out.print("List content: ");
-        SNode current = head;
+        DNode current = head;
         while (current != null) {
             System.out.print(current.data + " ");
             current = current.next;
@@ -194,7 +194,7 @@ public class SLL {
         if (size <= 1) {
             return true;
         }
-        SNode current = head;
+        DNode current = head;
         while (current.next != null) {
             if (current.data > current.next.data) {
                 return false;
@@ -209,28 +209,28 @@ public class SLL {
 
         // Test insertHead
         SLL sll = new SLL();
-        sll.insertHead(new SNode(1));
-        sll.insertHead(new SNode(2));
-        sll.insertHead(new SNode(3));
+        sll.insertHead(new DNode(1));
+        sll.insertHead(new DNode(2));
+        sll.insertHead(new DNode(3));
         sll.print(); // Expected output: List length: 3, Sorted status: false, List content: 3 2 1
 
         // Test insertTail
-        sll.insertTail(new SNode(4));
-        sll.insertTail(new SNode(5));
+        sll.insertTail(new DNode(4));
+        sll.insertTail(new DNode(5));
         sll.print(); // Expected output: List length: 5, Sorted status: false, List content: 3 2 1 4 5
 
         // Test insert at position
-        sll.insert(new SNode(6), 2);
-        sll.insert(new SNode(7), 6);
+        sll.insert(new DNode(6), 2);
+        sll.insert(new DNode(7), 6);
         sll.print(); // Expected output: List length: 7, Sorted status: false, List content: 3 6 2 1 4 5 7
 
         // Test sortedInsert
-        sll.sortedInsert(new SNode(0));
-        sll.sortedInsert(new SNode(8));
+        sll.sortedInsert(new DNode(0));
+        sll.sortedInsert(new DNode(8));
         sll.print(); // Expected output: List length: 9, Sorted status: true, List content: 0 1 2 3 4 5 6 7 8
 
         // Test search
-        SNode node = sll.search(4);
+        DNode node = sll.search(4);
         System.out.println(node.data); // Expected output: 4
 
         // Test deleteHead
