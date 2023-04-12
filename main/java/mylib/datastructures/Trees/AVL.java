f584e92adb4dfab137f94aff9496073335f041b9
MusaJawad <musa.jawad@ucalgary.ca>
2023-04-11 18:17:22 -0600

    Documentation for Tree

--- main/java/mylib/datastructures/Trees/AVL.java
+++ main/java/mylib/datastructures/Trees/AVL.java
@@ -1,87 +1,132 @@
 package main.java.mylib.datastructures.Trees;
 import main.java.mylib.datastructures.nodes.TNode;
 
-
+    /**
+     * AVL is a self-balancing binary search tree that extends the BST class.
+     * It uses rotations to maintain a balanced tree with a maximum height of log(n).
+     */
     public class AVL extends BST {
     
-        public AVL() {
-            super();
+         
+    /**
+     * Creates an empty AVL tree.
+     */
+    public AVL() {
+        super();
+    }
+    
+    /**
+     * Creates an AVL tree with a single node with the given value.
+     * 
+     * @param val the value of the node to be created
+     */
+    public AVL(int val) {
+        super(val);
+    }
+    
+    /**
+     * Creates an AVL tree with the given root node and balances the tree.
+     * 
+     * @param obj the root node of the tree
+     */
+    public AVL(TNode obj) {
+        super(obj);
+        balance(root);
+    }
+    
+    /**
+     * Balances the subtree rooted at the given node using rotations.
+     * 
+     * @param node the root of the subtree to be balanced
+     */
+    private void balance(TNode node) {
+        if (node == null) {
+            return;
         }
         
-        public AVL(int val) {
-            super(val);
-        }
+        int balanceFactor = getBalanceFactor(node);
         
-        public AVL(TNode obj) {
-            super(obj);
-            balance(root);
-        }
-        
-        private void balance(TNode node) {
-            if (node == null) {
-                return;
-            }
-            
-            int balanceFactor = getBalanceFactor(node);
-            
-            if (balanceFactor > 1) {
-                if (getBalanceFactor(node.left) >= 0) {
-                    node = rotateRight(node);
-                } else {
-                    node.left = rotateLeft(node.left);
-                    node = rotateRight(node);
-                }
-            } else if (balanceFactor < -1) {
-                if (getBalanceFactor(node.right) <= 0) {
-                    node = rotateLeft(node);
-                } else {
-                    node.right = rotateRight(node.right);
-                    node = rotateLeft(node);
-                }
-            }
-            
-            if (node.parent != null) {
-                balance(node.parent);
+        if (balanceFactor > 1) {
+            if (getBalanceFactor(node.left) >= 0) {
+                node = rotateRight(node);
             } else {
-                root = node;
+                node.left = rotateLeft(node.left);
+                node = rotateRight(node);
+            }
+        } else if (balanceFactor < -1) {
+            if (getBalanceFactor(node.right) <= 0) {
+                node = rotateLeft(node);
+            } else {
+                node.right = rotateRight(node.right);
+                node = rotateLeft(node);
             }
         }
         
-        private int getBalanceFactor(TNode node) {
-            if (node == null) {
-                return 0;
-            }
-            
-            int leftHeight = height(node.left);
-            int rightHeight = height(node.right);
-            
-            return leftHeight - rightHeight;
+        if (node.parent != null) {
+            balance(node.parent);
+        } else {
+            root = node;
+        }
+    }
+    
+    /**
+     * Returns the balance factor of the given node.
+     * 
+     * @param node the node whose balance factor is to be computed
+     * @return the balance factor of the node
+     */
+    private int getBalanceFactor(TNode node) {
+        if (node == null) {
+            return 0;
         }
         
-        private TNode rotateLeft(TNode node) {
-            TNode newRoot = node.right;
-            node.right = newRoot.left;
-            if (newRoot.left != null) {
-                newRoot.left.parent = node;
-            }
-            newRoot.left = node;
-            newRoot.parent = node.parent;
-            node.parent = newRoot;
-            return newRoot;
-        }
+        int leftHeight = height(node.left);
+        int rightHeight = height(node.right);
         
-        private TNode rotateRight(TNode node) {
-            TNode newRoot = node.left;
-            node.left = newRoot.right;
-            if (newRoot.right != null) {
-                newRoot.right.parent = node;
-            }
-            newRoot.right = node;
-            newRoot.parent = node.parent;
-            node.parent = newRoot;
-            return newRoot;
+        return leftHeight - rightHeight;
+    }
+    
+    /**
+     * Rotates the subtree rooted at the given node to the left.
+     * 
+     * @param node the root of the subtree to be rotated
+     * @return the new root of the subtree
+     */
+    private TNode rotateLeft(TNode node) {
+        TNode newRoot = node.right;
+        node.right = newRoot.left;
+        if (newRoot.left != null) {
+            newRoot.left.parent = node;
         }
-        
+        newRoot.left = node;
+        newRoot.parent = node.parent;
+        node.parent = newRoot;
+        return newRoot;
+    }
+    
+    /**
+     * Rotates the subtree rooted at the given node to the right.
+     * 
+     * @param node the root of the subtree to be rotated
+     * @return the new root of the subtree
+     */
+    private TNode rotateRight(TNode node) {
+        TNode newRoot = node.left;
+        node.left = newRoot.right;
+        if (newRoot.right != null) {
+            newRoot.right.parent = node;
+        }
+        newRoot.right = node;
+        newRoot.parent = node.parent;
+        node.parent = newRoot;
+        return newRoot;
+    }
+
+
+        /**
+        Inserts a new integer value into the AVL tree.
+        @param val the integer value to insert
+        */
         @Override
         public void insert(int val) {
             TNode newNode = new TNode(val, 0, null, null, null);
@@ -110,17 +155,29 @@ import main.java.mylib.datastructures.nodes.TNode;
                 }
             }
         }
+
+        
+        /**
+        Updates the balance of the tree after inserting a new node.
+        @param node the node that was just inserted
+        */
+                
         private void updateBalance(TNode node) {
             if (node == null) {
                 return;
             }
         }
-        
+
+        /**
+        Inserts a new TNode object into the AVL tree.
+        @param node the TNode object to insert
+        */
         @Override
         public void insert(TNode node) {
             super.insert(node);
             balance(node);
         }
+        
         
         private int height(TNode node) {
             if (node == null) {
@@ -132,7 +189,15 @@ import main.java.mylib.datastructures.nodes.TNode;
     
     
 
-    
+        /**
+     * Recursive method to search for a node with the given value in the AVL tree.
+     * Starting from the specified node, the method will traverse the tree
+     * recursively to find the node with the specified value.
+     *
+     * @param val the value to search for
+     * @param node the starting node for the search
+     * @return the node with the specified value, or null if not found
+     */
     private TNode getNode(int val, TNode node) {
         if (node == null || node.data == val) {
             return node;
@@ -142,7 +207,16 @@ import main.java.mylib.datastructures.nodes.TNode;
             return getNode(val, node.left);
         }
     }
-    
+
+    /**
+     * Public method to search for a node with the given value in the AVL tree.
+     * Starting from the root node, the method will traverse the tree recursively
+     * to find the node with the specified value.
+     *
+     * @param val the value to search for
+     * @return the node with the specified value, or null if not found
+     */
+        
     public TNode getNode(int val) {
         return getNode(val, root);
     }
